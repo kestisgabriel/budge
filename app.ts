87ba99bc81d3120a59dc1ff038f313e2386f9049
Gabriel <priv@gabrielnyberg.com>
2024-08-11 10:01:55 +0200

    define expenses endpoint

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { expensesRoute } from "./routes/expenses";
 
 const app = new Hono();
 
@@ -9,4 +10,6 @@ app.use(logger());
 // c: context object = handles http requests and responses
 app.get("/", (c) => c.text("Hono!"));
 
+// if /api/expenses, expensesRoute handles http request
+app.route("/api/expenses", expensesRoute);
 export default app;
