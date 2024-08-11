179656ae54192c4e1e11eebc72f014c5dfb3ef68
Gabriel <priv@gabrielnyberg.com>
2024-08-11 09:53:10 +0200

    hono logger

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 const app = new Hono();
 
+app.use(logger());
+// c: context object = handles http requests and responses
 app.get("/", (c) => c.text("Hono!"));
 
 export default app;
