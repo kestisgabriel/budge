d161454a5e1c814c235ad5fb0a14c4d0edc8befb
Gabriel <priv@gabrielnyberg.com>
2024-08-11 09:53:50 +0200

    comments

--- app.ts
+++ app.ts
@@ -3,7 +3,9 @@ import { logger } from "hono/logger";
 
 const app = new Hono();
 
+// logger middleware
 app.use(logger());
+
 // c: context object = handles http requests and responses
 app.get("/", (c) => c.text("Hono!"));
 
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 
 Bun.serve({
+	// hono server
 	fetch: app.fetch,
 });
 
