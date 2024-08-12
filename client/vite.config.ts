64d046a6c1bda8d113dd56470ffb6ef6a8e0d3d0
Gabriel <priv@gabrielnyberg.com>
2024-08-12 16:13:31 +0200

    init tanstack router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,12 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), TanStackRouterVite(), viteReact()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
