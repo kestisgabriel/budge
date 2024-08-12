262f3bb88bc030b167fd0d0b4cb91a08b7deb288
Gabriel <priv@gabrielnyberg.com>
2024-08-12 15:24:35 +0200

    first build

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,17 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 // logger middleware
 app.use(logger());
 
-// c: context object = handles http requests and responses
-app.get("/", (c) => c.text("Hono!"));
-
 // if /api/expenses, expensesRoute handles http request
 app.route("/api/expenses", expensesRoute);
+
+app.get("*", serveStatic({ root: "./client/dist" }));
+app.get("*", serveStatic({ path: "./client/dist/index.html" }));
+
 export default app;
