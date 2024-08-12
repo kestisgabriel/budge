eaeb6ce92424e79afca2334562daa0c31cf1b8e3
Gabriel <priv@gabrielnyberg.com>
2024-08-12 17:35:24 +0200

    added skeleton ui

--- client/src/routes/expenses.tsx
+++ client/src/routes/expenses.tsx
@@ -6,11 +6,11 @@ import {
 	TableBody,
 	TableCaption,
 	TableCell,
-	TableFooter,
 	TableHead,
 	TableHeader,
 	TableRow
 } from '@/components/ui/table'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export const Route = createFileRoute('/expenses')({
 	component: Expenses
@@ -46,7 +46,21 @@ function Expenses() {
 				</TableHeader>
 				<TableBody>
 					{isPending
-						? '...'
+						? Array(3)
+								.fill(0)
+								.map((_, i) => (
+									<TableRow key={i}>
+										<TableCell className="font-medium">
+											<Skeleton className="h-4" />
+										</TableCell>
+										<TableCell>
+											<Skeleton className="h-4" />
+										</TableCell>
+										<TableCell>
+											<Skeleton className="h-4" />
+										</TableCell>
+									</TableRow>
+								))
 						: data?.expenses.map((expense) => (
 								<TableRow key={expense.id}>
 									<TableCell className="font-medium">
