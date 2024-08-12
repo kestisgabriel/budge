765da5040b6cf947d2cf26bd3a39de7444ac3cb3
Gabriel <priv@gabrielnyberg.com>
2024-08-12 16:19:14 +0200

    updated main.tsx to call routerprovider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
 	<QueryClientProvider client={queryClient}>
 		<StrictMode>
-			<App />
+			<RouterProvider router={router} />
 		</StrictMode>
 	</QueryClientProvider>
 );
