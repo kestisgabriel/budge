d345ba5ab7d0d0074603100b8bbd510b35b5e560
Gabriel <priv@gabrielnyberg.com>
2024-08-12 17:30:35 +0200

    shadcn table

--- client/src/routes/expenses.tsx
+++ client/src/routes/expenses.tsx
@@ -1,6 +1,16 @@
 import { api } from '@/lib/api'
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import {
+	Table,
+	TableBody,
+	TableCaption,
+	TableCell,
+	TableFooter,
+	TableHead,
+	TableHeader,
+	TableRow
+} from '@/components/ui/table'
 
 export const Route = createFileRoute('/expenses')({
 	component: Expenses
@@ -24,8 +34,32 @@ function Expenses() {
 	if (error) return 'An error has occurred: ' + error.message
 
 	return (
-		<div>
-			<pre>{isPending ? '...' : JSON.stringify(data, null, 2)}</pre>
+		<div className="p-2 max-w-3xl m-auto">
+			<Table>
+				<TableCaption>A list of your expenses.</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead className="w-[100px]">ID</TableHead>
+						<TableHead>Title</TableHead>
+						<TableHead className="text-right">Amount</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					{isPending
+						? '...'
+						: data?.expenses.map((expense) => (
+								<TableRow key={expense.id}>
+									<TableCell className="font-medium">
+										{expense.title}
+									</TableCell>
+									<TableCell>{expense.amount}</TableCell>
+									<TableCell className="text-right">
+										{expense.amount}
+									</TableCell>
+								</TableRow>
+							))}
+				</TableBody>
+			</Table>
 		</div>
 	)
 }
