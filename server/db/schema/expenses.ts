c7d3599f00a4de36ba21955efa2648034c521c43
Gabriel <priv@gabrielnyberg.com>
2024-08-15 21:54:36 +0200

    refined expenses schema

--- server/db/schema/expenses.ts
+++ server/db/schema/expenses.ts
@@ -3,9 +3,8 @@ import {
 	numeric,
 	pgTable,
 	serial,
-	uniqueIndex,
-	varchar,
-	index
+	index,
+	timestamp
 } from 'drizzle-orm/pg-core'
 
 export const expenses = pgTable(
@@ -14,7 +13,8 @@ export const expenses = pgTable(
 		id: serial('id').primaryKey(),
 		userId: text('user_id').notNull(),
 		title: text('title').notNull(),
-		amount: numeric('amount', { precision: 12, scale: 2 }).notNull()
+		amount: numeric('amount', { precision: 12, scale: 2 }).notNull(),
+		createdAt: timestamp('created-at').defaultNow()
 	},
 	(expenses) => {
 		return {
