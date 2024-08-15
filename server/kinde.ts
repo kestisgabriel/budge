2dc5408608c5f53da8f6344acb94d1a489f03f7a
Gabriel <priv@gabrielnyberg.com>
2024-08-15 06:31:53 +0200

    kinde init

---
+++ server/kinde.ts
@@ -0,0 +1,13 @@
+import {
+	createKindeServerClient,
+	GrantType
+} from '@kinde-oss/kinde-typescript-sdk'
+
+// Client for authorization code flow
+const kindeClient = createKindeServerClient(GrantType.AUTHORIZATION_CODE, {
+	authDomain: 'process.env.KINDE_DOMAIN',
+	clientId: 'process.env.KINDE_CLIENT_ID',
+	clientSecret: 'process.env.KINDE_CLIENT_SECRET',
+	redirectURL: 'process.env.KINDE_REDIRECT_URL',
+	logoutRedirectURL: 'process.env.KINDE_LOGOUT_REDIRECT_URL'
+})
