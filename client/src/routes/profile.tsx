658c867bf014e1386f4b4eebd85113f0fdbd7dda
Gabriel <priv@gabrielnyberg.com>
2024-08-15 09:29:14 +0200

    refactored userQueryOptions

--- client/src/routes/profile.tsx
+++ client/src/routes/profile.tsx
@@ -1,25 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
-import { api } from '@/lib/api'
+import { userQueryOptions } from '@/lib/api'
 
 export const Route = createFileRoute('/profile')({
 	component: Profile
 })
 
-async function getCurrentUser() {
-	const res = await api.me.$get()
-	if (!res.ok) {
-		throw new Error('server error')
-	}
-	const data = await res.json()
-	return data
-}
-
 function Profile() {
-	const { isPending, error, data } = useQuery({
-		queryKey: ['get-current-user'],
-		queryFn: getCurrentUser
-	})
+	const { isPending, error, data } = useQuery(userQueryOptions)
+
 	if (isPending) return 'loading'
 	if (error) return 'not logged in'
 
