d26ce5f80e74a45ab47aa5494e91a088d04d310c
Gabriel <priv@gabrielnyberg.com>
2024-08-15 09:19:24 +0200

    created function for fetching user

--- client/src/routes/profile.tsx
+++ client/src/routes/profile.tsx
@@ -1,9 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useQuery } from '@tanstack/react-query'
+import { api } from '@/lib/api'
 
 export const Route = createFileRoute('/profile')({
 	component: Profile
 })
 
+async function getCurrentUser() {
+	const res = await api.me.$get()
+	if (!res.ok) {
+		throw new Error('server error')
+	}
+	const data = await res.json()
+	return data
+}
+
 function Profile() {
-	return <div className="p-2">Hello from Profile!</div>
+	const { isPending, error, data } = useQuery({
+		queryKey: ['get-current-user'],
+		queryFn: getCurrentUser
+	})
+	if (isPending) return 'loading'
+	if (error) return 'not logged in'
+
+	return <div className="p-2">Hello {data.user.family_name}</div>
 }
