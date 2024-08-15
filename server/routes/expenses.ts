12bad97d7f9d9decd4bec2f53a86ab81676e3b6c
Gabriel <priv@gabrielnyberg.com>
2024-08-15 21:09:38 +0200

    preparing post and get for db integration

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -2,11 +2,14 @@ import { Hono } from 'hono'
 import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
 import { getUser } from '../kinde'
+import { db } from '../db'
+import { expenses as expenseTable } from '../db/schema/expenses'
+import { eq } from 'drizzle-orm'
 
 const expenseSchema = z.object({
 	id: z.number().int().positive().min(1),
 	title: z.string().min(3).max(100),
-	amount: z.number().int().positive()
+	amount: z.string()
 })
 
 type Expense = z.infer<typeof expenseSchema>
@@ -14,26 +17,40 @@ type Expense = z.infer<typeof expenseSchema>
 const createExpenseSchema = expenseSchema.omit({ id: true })
 
 const dummyExpenses: Expense[] = [
-	{ id: 1, title: 'Groceries', amount: 120.5 },
-	{ id: 2, title: 'Rent', amount: 950.0 },
-	{ id: 3, title: 'Utilities', amount: 75.3 }
+	{ id: 1, title: 'Groceries', amount: '120.5' },
+	{ id: 2, title: 'Rent', amount: '950.0' },
+	{ id: 3, title: 'Utilities', amount: '75.3' }
 ]
 
 // expense routes defined
 export const expensesRoute = new Hono()
 	.get('/', getUser, async (c) => {
 		const user = c.var.user
-		return c.json({ expenses: dummyExpenses })
+
+		const expenses = await db
+			.select()
+			.from(expenseTable)
+			.where(eq(expenseTable.userId, user.id))
+		return c.json({ expenses: expenses })
 	})
 	.post('/', getUser, zValidator('json', createExpenseSchema), async (c) => {
 		const expense = await c.req.valid('json')
-		dummyExpenses.push({ ...expense, id: dummyExpenses.length + 1 })
+		const user = c.var.user
+
+		const result = await db
+			.insert(expenseTable)
+			.values({
+				...expense,
+				userId: user.id
+			})
+			.returning()
+
 		c.status(201)
-		return c.json(expense)
+		return c.json(result)
 	})
 	.get('/total-spent', getUser, async (c) => {
 		const totalSpent = dummyExpenses.reduce(
-			(acc, expense) => acc + expense.amount,
+			(acc, expense) => acc + +expense.amount,
 			0
 		)
 		return c.json({ totalSpent })
