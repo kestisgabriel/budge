35dc803c191c1b4131733a72fcc29ce4c8f62d97
Gabriel <priv@gabrielnyberg.com>
2024-08-16 07:07:17 +0200

    zod schema error messages

--- server/formSchemas.ts
+++ server/formSchemas.ts
@@ -2,7 +2,14 @@ import { z } from 'zod'
 
 export const expenseSchema = z.object({
 	id: z.number().int().positive().min(1),
-	title: z.string().min(3).max(100),
+	title: z
+		.string()
+		.min(3, {
+			message: 'Title must be at least 3 characters'
+		})
+		.max(100, {
+			message: 'Title can not exceed 100 characters'
+		}),
 	amount: z.string()
 })
 
